6a62e3b4be0c0d9a4acc7b008d6962b945580fc3
Adam <caiyijias@outlook.com>
2019-08-08 20:56:47 +0800

    设置routers

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -2,15 +2,33 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/learn">课程学习</router-link> |
+      <router-link to="/student">学员展示</router-link> |
+      <router-link to="/community">社区</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div class="header">
+      <div class="center">
+        <div class="logo">
+          <img src="./assets/logo.png" alt="">
+        </div>
+        <div class="nav">
+          <router-link tag="li" to="/">Home</router-link> |
+          <router-link tag="li" to="/learn">课程学习</router-link> |
+          <router-link tag="li" to="/student">学员展示</router-link> |
+          <router-link tag="li" to="/community">社区</router-link> |
+          <router-link tag="li" to="/about">About</router-link>
+        </div>
+      </div>
+    </div>
+
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+
 Vue.use(Router)
 
 export default new Router({
@@ -14,12 +15,56 @@ export default new Router({
       component: Home
     },
     {
+      path: '/learn',
+      name: 'learn',
+      component: () => import('./views/Learn')
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: () => import('./views/Student')
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/About')
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: () => import('./views/Community'),
+      // redirect: '/community/academic',
+      // children: [
+      //   {
+      //     path: 'academic',
+      //     name: 'academic',
+      //     component: () => import('./views/Academic')
+      //   },
+      //   {
+      //     path: 'download',
+      //     name: 'download',
+      //     component: () => import('./views/Download')
+      //   },
+      //   {
+      //     path: 'personal',
+      //     name: 'personal',
+      //     component: () => import('./views/Personal')
+      //   }
+      // ]
+    },
+    // {
+    //   path: '/NotFound',
+    //   component: () => import('./views/NotFound')
+    // },
+    {
+      path: '*',
+      redirect (to) {
+        if(to.path === '/') {
+          return '/home';
+        } else {
+          return '/NotFound';
+        }
+      }
     }
   ]
 })
