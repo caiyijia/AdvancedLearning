27d6a7128c2ed53fe3eb881d5cec25a64c001dc2
Adam <caiyijias@outlook.com>
2019-08-08 20:25:04 +0800

    初识路由

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 console.log(App)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
