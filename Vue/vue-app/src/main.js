c04c8bf88523d6a98b54e8cb6c9923ff177052ff
Adam <caiyijias@outlook.com>
2019-08-10 00:28:04 +0800

    add vuex

--- Vue/vue-app/src/main.js
+++ Vue/vue-app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/reset.css'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -9,5 +10,6 @@ Vue.prototype.bus = new Vue()
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
