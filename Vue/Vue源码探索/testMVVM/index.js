4bd3fb4596f8f861fb71032892977b1b7a126701
Adam <caiyijias@outlook.com>
2019-08-11 01:21:36 +0800

    用Object.defineProperty实现Vue双向数据绑定

--- Vue/Vue源码探索/testMVVM/index.js
+++ Vue/Vue源码探索/testMVVM/index.js
@@ -18,12 +18,42 @@ function render(el, originTemplate, templates, data) {
     el.innerHTML = result;
 }
 
+function deepClone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+function proxyObj(obj, newObj) {
+    var self = this;
+    for(let temp in obj) {
+        if(temp instanceof Object){
+            proxyObj(obj[temp], newObj[temp])
+        }else {
+            Object.defineProperty(obj, temp, {
+                get: function () {
+                    // console.log('---');
+                    return newObj[temp];
+                },
+                set: function (value) {
+                    console.log('+++');
+                    newObj[temp] = value;
+                    // 此时的this是被调用时的data数据对象
+                    // console.log(this)
+                    render(self.el, self.originTemplate, self.templates, self.data)
+                }
+            })
+        }
+        
+    }
+}
+
+
 function MyMVVM(id, data) {
     this.id = id;
     this.data = data;
     this.el = document.getElementById(this.id);
     this.originTemplate = this.el.innerHTML;
     this.templates = analysisTemplate(this.el.innerHTML);
+    this.cloneObj = deepClone(this.data);
+    proxyObj.call(this, this.data, this.cloneObj);
     render(this.el, this.originTemplate, this.templates, this.data)
 }
 
--- Vue/Vue源码探索/testMVVM/test.js
+++ Vue/Vue源码探索/testMVVM/test.js
@@ -1,37 +1,44 @@
 var obj = {
     x: 1,
-    y: 2
+    y: 2,
+    z: {
+        a: 3,
+        b: 4
+    }
 }
 
 function deepClone(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
 
-
-
-function proxyObj(obj) {
-    let newObj = deepClone(obj);
+var newObj = deepClone(obj);
+function proxyObj(obj, newObj) {
     for(let temp in obj) {
-        Object.defineProperty(obj, temp, {
-            get: function () {
-                console.log('---');
-                return newObj[temp];
-            },
-            set: function (value) {
-                console.log('+++');
-                newObj[temp] = value;
-            }
-        })
+        if(temp instanceof Object){
+            proxyObj(obj[temp], newObj[temp])
+        }else {
+            Object.defineProperty(obj, temp, {
+                get: function () {
+                    console.log('---');
+                    return newObj[temp];
+                },
+                set: function (value) {
+                    console.log('+++');
+                    newObj[temp] = value;
+                }
+            })
+        }
+        
     }
 }
-proxyObj(obj)
-obj.x = 3
+proxyObj(obj, newObj)
+obj.z.a = 3
 // console.log(obj.x);
 
-var proxy = new Proxy(obj, {
-    get: function(obj, x) {
-        console.log('----')
-        return Reflect.get(obj, 'x');
-    }
-})
-console.log(proxy.x)
+// var proxy = new Proxy(obj, {
+//     get: function(obj, x) {
+//         console.log('----')
+//         return Reflect.get(obj, 'x');
+//     }
+// })
+console.log(obj.z.b)
