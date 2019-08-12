8f2ed9ce806cfed1e06085cd3fa0b77290173032
Adam <caiyijias@outlook.com>
2019-08-12 20:31:51 +0800

    function Stateless Components

---
+++ React/react-app/src/components/TagList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+// function component 无状态组件 Stateless Components 无生命周期 仅仅展示的时候使用
+function Taglist(props) {
+    console.log(this)
+    const {list} = props;
+    return(
+            <div className="wrapper">
+                <div className="search-title-box">
+                <h5 className="search-title">搜索热点</h5>
+                <span className="refresh">换一换</span>
+                </div>
+                <ul className="top-list-container">
+                {
+                    list.map( (item, index) => {
+                        const indexStyle = {};
+                        switch (index) {
+                            case 0:
+                                indexStyle.backgroundColor = '#f54545';
+                                break;
+                            case 1:
+                                indexStyle.backgroundColor = '#ff8547';
+                                break;
+                            case 2:
+                                indexStyle.backgroundColor = '#ffac38';
+                                break;
+                            default:
+                                break;
+                            }
+                        return (
+                            <li className="top-list" key={item.id}>
+                    <div className="top-title">
+                    <span className="hot-index" style={indexStyle}>{index + 1}</span>
+                    <a className="topic-title">{item.title}</a>
+                    {item.new ? <span className="topic-new">新</span> : null}
+                    </div>
+                    <div className="hot-degree">
+                    <span>{item.hot}</span>
+                    </div>
+                </li>
+                        )
+                    })
+                }
+                </ul>
+            </div>
+        )
+    
+}
+
+export default Taglist;
--- React/react-app/src/index.js
+++ React/react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css'
+import './index.css';
+import Taglist from './components/TagList';
 
 // { js } <> html <>
 // function test() {
@@ -79,47 +80,53 @@ const topList = [
     }
   ]
 
-let element =(
-    <div className="wrapper">
-        <div className="search-title-box">
-        <h5 className="search-title">搜索热点</h5>
-        <span className="refresh">换一换</span>
-        </div>
-        <ul className="top-list-container">
-        {
-            topList.map( (item, index) => {
-                const indexStyle = {};
-                switch (index) {
-                    case 0:
-                        indexStyle.backgroundColor = '#f54545';
-                        break;
-                    case 1:
-                        indexStyle.backgroundColor = '#ff8547';
-                        break;
-                    case 2:
-                        indexStyle.backgroundColor = '#ffac38';
-                        break;
-                    default:
-                        break;
-                    }
-                return (
-                    <li className="top-list" key={item.id}>
-            <div className="top-title">
-            <span className="hot-index" style={indexStyle}>{index + 1}</span>
-            <a className="topic-title">{item.title}</a>
-            {item.new ? <span className="topic-new">新</span> : null}
-            </div>
-            <div className="hot-degree">
-            <span>{item.hot}</span>
-            </div>
-        </li>
-                )
-            })
-        }
-        </ul>
-    </div>
-)
+// let element =(
+//     <div className="wrapper">
+//         <div className="search-title-box">
+//         <h5 className="search-title">搜索热点</h5>
+//         <span className="refresh">换一换</span>
+//         </div>
+//         <ul className="top-list-container">
+//         {
+//             topList.map( (item, index) => {
+//                 const indexStyle = {};
+//                 switch (index) {
+//                     case 0:
+//                         indexStyle.backgroundColor = '#f54545';
+//                         break;
+//                     case 1:
+//                         indexStyle.backgroundColor = '#ff8547';
+//                         break;
+//                     case 2:
+//                         indexStyle.backgroundColor = '#ffac38';
+//                         break;
+//                     default:
+//                         break;
+//                     }
+//                 return (
+//                     <li className="top-list" key={item.id}>
+//             <div className="top-title">
+//             <span className="hot-index" style={indexStyle}>{index + 1}</span>
+//             <a className="topic-title">{item.title}</a>
+//             {item.new ? <span className="topic-new">新</span> : null}
+//             </div>
+//             <div className="hot-degree">
+//             <span>{item.hot}</span>
+//             </div>
+//         </li>
+//                 )
+//             })
+//         }
+//         </ul>
+//     </div>
+// )
+
+function Component() {
+    return (
+        <div>component</div>
+    )
+}
 
 
-
-render(element, document.getElementById('root'));
+// React 中的组件要大写
+render(<><Taglist list={topList}></Taglist><Taglist list={topList}></Taglist></>, document.getElementById('root'));
