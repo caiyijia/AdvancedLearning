1e6ec3beba1cf01b92961c87adee4e84d8cf45c0
Adam <caiyijias@outlook.com>
2019-08-14 17:47:13 +0800

    finish login button

--- React/react-app-1/src/pages/login/Login.js
+++ React/react-app-1/src/pages/login/Login.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-
+import './style.css'
 class Login extends Component {
-
-  render () {
+  state = {
+    isLogin: document.cookie.includes('login=true')
+  }
+  render() {
     return (
-      <div>登录</div>
+      <div className='login'>
+        <button className='login-btn' onClick={this.handleClick}>
+          {this.state.isLogin ? '注销' : '登陆'}</button>
+      </div>
     )
+  }
+
+  handleClick = () => {
+    const isLogin = this.state.isLogin;
+    isLogin ? this.setCookie('login', '', -1) : this.setCookie('login', true, 7);
+    this.setState({
+      isLogin: !isLogin
+    })
+  }
+
+  setCookie = (key, value, day) => {
+    const expires = day * 86400
+    const data = new Date(+new Date + expires)
+    document.cookie = `${key}=${value}; expires=${data.toUTCString()}`
   }
 
 }
