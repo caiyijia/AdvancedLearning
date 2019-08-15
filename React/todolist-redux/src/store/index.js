cc3412fd8b69552b6f0fd584400d2410cfad6a3f
Adam <caiyijias@outlook.com>
2019-08-16 02:08:59 +0800

    composeEnhancers

--- React/todolist-redux/src/store/index.js
+++ React/todolist-redux/src/store/index.js
@@ -1,11 +1,14 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from './reducers'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise'
 import logger from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [ thunk, promise, logger ]
 const store = createStore(
     reducer,
-    applyMiddleware(thunk, promise, logger)
+    composeEnhancers(applyMiddleware(...middleware))
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
