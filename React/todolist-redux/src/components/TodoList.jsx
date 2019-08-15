bd2acc9c96da8773a32a53ff0b395bfda7ccfc59
Adam <caiyijias@outlook.com>
2019-08-16 01:41:29 +0800

    axios.interceptors.request.use

--- React/todolist-redux/src/components/TodoList.jsx
+++ React/todolist-redux/src/components/TodoList.jsx
@@ -11,11 +11,17 @@ axios.interceptors.response.use(res => {
     return Promise.reject('请求出错')
 })
 
+axios.interceptors.request.use(config => {
+    console.log(config);
+    config.headers.token = Math.random().toFixed(15) * 1e15;
+    return config
+})
+
 class TodoList extends Component {
 
     componentDidMount () {
         axios.get('/list.json').then(res => {
-            console.log(res);
+            // console.log(res);
             this.props.getInitList(res)
             // const list = res.data.data
             // this.props.getInitList(list)
