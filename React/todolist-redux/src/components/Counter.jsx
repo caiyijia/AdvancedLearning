4cc35ad438853ce72d63ab2fd7baa4bc5bbaa7ae
Adam <caiyijias@outlook.com>
2019-08-15 11:46:55 +0800

    adjust Counter using react-redux

--- React/todolist-redux/src/components/Counter.jsx
+++ React/todolist-redux/src/components/Counter.jsx
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
 import store from '../store'
 import { getCountAddAction } from '../store/actions/counter'
-class CCC extends Component {
-
-    state = store.getState().counter
-    
-
-    componentDidMount() {
-        store.subscribe(() => {
-            this.setState(
-                store.getState().counter
-            )
-        })
-    }
+import { connect } from 'react-redux'
+class counter extends Component {
 
     render () {
         return (
             <div>
-                {this.state.count}
+                {this.props.count}
                 <button onClick={this.handleClick}>add</button>
             </div>
         )
     }
 
-    handleClick = (n) => {
-        const action = getCountAddAction(3)
-        store.dispatch(action)
+    handleClick = () => {
+        this.props.add(1)
     }
 
 }
 
-export default CCC;
+const mapStateToProps = (state) => ({
+    count: state.counter.count
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    add: (n) => {
+        dispatch(getCountAddAction(n))
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(counter);
--- React/todolist-redux/src/index.js
+++ React/todolist-redux/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from 'react-dom'
 import TodoList from './components/TodoList.jsx'
 import Counter from './components/Counter'
+import { Provider } from 'react-redux'
+import store from './store'
 
-render(<><Counter /><TodoList /></>, document.getElementById('root'));
+render(<Provider store={store}>
+    <Counter />
+    {/* <TodoList /> */}
+    </Provider>, document.getElementById('root'));
 
