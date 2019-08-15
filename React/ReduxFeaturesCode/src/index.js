4a98cab492147dc3f9fdc91d85aae23883ad2243
Adam <caiyijias@outlook.com>
2019-08-15 15:46:13 +0800

    实现基本的redux

--- React/ReduxFeaturesCode/src/index.js
+++ React/ReduxFeaturesCode/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import TodoList from './components/TodoList';
+import Input from './components/Input'
 
-render(<TodoList></TodoList>, window.root);
+render(<><TodoList></TodoList><Input></Input></>, window.root);
