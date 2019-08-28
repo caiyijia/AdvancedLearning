38c445d58d1d30e5b4e6b11af1d390590fa73937
Adam <caiyijias@outlook.com>
2019-08-28 18:58:26 +0800

    update

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,27 +1,27 @@
-var net = require("net");
+var net  = require("net");//网络层和运输层   TCP/IP协议
+var fs = require("fs");
+var globalConf = require("./conf");
 
 var server = net.createServer();
-server.listen(12306, "127.0.0.1");
+server.listen(globalConf.port, "127.0.0.1");
 
 server.on("listening", function () {
     console.log("服务已启动");
 });
 
-server.on("connection", function (socket) {
-    console.log("有新的链接");
-
+server.on("connection", function(socket) {
     socket.on("data", function (data) {
+
         console.log(data.toString());
-        socket.write("HTTP 200OK");
+
+        var url = data.toString().split("\r\n")[0].split(" ")[1];
+        try {
+            var dataFile = fs.readFileSync(globalConf["basePath"] + url);
+            socket.write("HTTP/1.1 200OK\r\n\r\n");
+            socket.write(dataFile);
+        } catch (e) {
+            socket.write("HTTP/1.1 404NotFound\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>");
+        }
+        socket.end();
     });
-
-    // socket.on("close", function () {
-    //     console.log("客户端已关闭");
-    //     server.close();
-    // });
-
 });
-//
-// server.on("close", function () {
-//     console.log("服务已关闭");
-// });
